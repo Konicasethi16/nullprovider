0c0f4b350d7219061592ed6fc72c9765aa6e7654
Konicasethi16 <167297988+Konicasethi16@users.noreply.github.com>
2024-10-01 15:42:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,3 +23,133 @@ resource "null_resource" "example5" {
     command = "echo This command will execute whenever the configuration changes-5"
   }
 }
+resource "null_resource" "example5" {
+  provisioner "local-exec" {
+    command = "echo This command will execute whenever the configuration changes-5"
+  }
+}
+resource "null_resource" "example6" {
+  provisioner "local-exec" {
+    command = "echo This command will execute whenever the configuration changes-6"
+  }
+}
+resource "null_resource" "example7" {
+  provisioner "local-exec" {
+    command = "echo This command will execute whenever the configuration changes-7"
+  }
+}
+resource "null_resource" "example8" {
+  provisioner "local-exec" {
+    command = "echo This command will execute whenever the configuration changes-5"
+  }
+}
+resource "null_resource" "example9" {
+  provisioner "local-exec" {
+    command = "echo This command will execute whenever the configuration changes-5"
+  }
+}
+resource "null_resource" "example10" {
+  provisioner "local-exec" {
+    command = "echo This command will execute whenever the configuration changes-5"
+  }
+}
+resource "null_resource" "example11" {
+  provisioner "local-exec" {
+    command = "echo This command will execute whenever the configuration changes-5"
+  }
+}
+resource "null_resource" "example12" {
+  provisioner "local-exec" {
+    command = "echo This command will execute whenever the configuration changes-5"
+  }
+}
+resource "null_resource" "example13" {
+  provisioner "local-exec" {
+    command = "echo This command will execute whenever the configuration changes-5"
+  }
+}
+resource "null_resource" "example14" {
+  provisioner "local-exec" {
+    command = "echo This command will execute whenever the configuration changes-5"
+  }
+}
+resource "null_resource" "example15" {
+  provisioner "local-exec" {
+    command = "echo This command will execute whenever the configuration changes-5"
+  }
+}
+resource "null_resource" "example16" {
+  provisioner "local-exec" {
+    command = "echo This command will execute whenever the configuration changes-5"
+  }
+}
+resource "null_resource" "example17" {
+  provisioner "local-exec" {
+    command = "echo This command will execute whenever the configuration changes-5"
+  }
+}
+resource "null_resource" "example18" {
+  provisioner "local-exec" {
+    command = "echo This command will execute whenever the configuration changes-5"
+  }
+}
+resource "null_resource" "example19" {
+  provisioner "local-exec" {
+    command = "echo This command will execute whenever the configuration changes-5"
+  }
+}
+resource "null_resource" "example20" {
+  provisioner "local-exec" {
+    command = "echo This command will execute whenever the configuration changes-5"
+  }
+}
+resource "null_resource" "example21" {
+  provisioner "local-exec" {
+    command = "echo This command will execute whenever the configuration changes-5"
+  }
+}
+resource "null_resource" "example22" {
+  provisioner "local-exec" {
+    command = "echo This command will execute whenever the configuration changes-5"
+  }
+}
+resource "null_resource" "example23" {
+  provisioner "local-exec" {
+    command = "echo This command will execute whenever the configuration changes-5"
+  }
+}
+resource "null_resource" "example24" {
+  provisioner "local-exec" {
+    command = "echo This command will execute whenever the configuration changes-5"
+  }
+}
+resource "null_resource" "example25" {
+  provisioner "local-exec" {
+    command = "echo This command will execute whenever the configuration changes-5"
+  }
+}
+resource "null_resource" "example26" {
+  provisioner "local-exec" {
+    command = "echo This command will execute whenever the configuration changes-5"
+  }
+}
+resource "null_resource" "example27" {
+  provisioner "local-exec" {
+    command = "echo This command will execute whenever the configuration changes-5"
+  }
+}
+resource "null_resource" "example28" {
+  provisioner "local-exec" {
+    command = "echo This command will execute whenever the configuration changes-5"
+  }
+}
+resource "null_resource" "example29" {
+  provisioner "local-exec" {
+    command = "echo This command will execute whenever the configuration changes-5"
+  }
+}
+resource "null_resource" "example30" {
+  provisioner "local-exec" {
+    command = "echo This command will execute whenever the configuration changes-5"
+  }
+}
